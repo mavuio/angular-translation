c494bb5e026593b70e7f61379b395902a7bbdaef
Manfred Wuits <manfred@monochrom.at>
2014-02-04 20:00:50 +0100

    silverstripe-pages included

--- src/Werkzeugh/AngularTranslation/AngularTranslationController.php
+++ src/Werkzeugh/AngularTranslation/AngularTranslationController.php
@@ -8,21 +8,60 @@ class AngularTranslationController extends BaseController {
 
 
 
+
+  public function __construct(/*LanguageProvider $languageProvider, LanguageEntryProvider $languageEntryProvider*/)
+  {
+    $this->setProviders(\App::make('Werkzeugh\TranslationAdmin\LanguageProvider'), \App::make('Werkzeugh\TranslationAdmin\LanguageEntryProvider'));
+  }
+
+  protected $languageProvider;
+  protected $languageEntryProvider;
+
+   protected function setProviders($languageProvider, $languageEntryProvider)
+  {
+    $this->languageProvider       = $languageProvider;
+    $this->languageEntryProvider  = $languageEntryProvider;
+  }
+
+  public function getAvailableLanguages()
+  {
+    static $langs;
+    if(!$langs)
+    {
+      foreach ($this->languageProvider->findAll() as $value) {
+        $langs[$value->locale]=$value->getAttributes();
+      }
+    }
+    return $langs;
+  }
+
   public function getJson()
   {
 
+      $model = $this->languageEntryProvider->createModel();
 
-    return '
-    {
-      "HEADLINE": "What an awesome module!",
-      "PARAGRAPH": "Srsly, ➜ [{{username}}]",
-      "NAMESPACE": {
-        "PARAGRAPH": "And it comes with awesome features!"
+
+      $langid=$this->getAvailableLanguages()[Input::get('lang')]['id'];
+
+      $query=DB::table($model->getTable());
+      $query->whereLanguageId($langid);
+
+      // $GLOBALS['debugsql']=1;
+
+
+        DB::connection()->setFetchMode(\PDO::FETCH_ASSOC);
+
+      $res=$query->get(['namespace','group','item','text']);
+
+      foreach ($res as $row) {
+        if($row['namespace']=="*")
+          $ret[$row['group']][$row['item']]=$row['text'];
+        else
+          $ret[$row['namespace']][$row['group']][$row['item']]=$row['text'];
       }
-    }
-    ';
 
 
+      return Response::json($ret);
 
   }
 }
